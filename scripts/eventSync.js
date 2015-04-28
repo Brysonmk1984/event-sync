da98a6de74aef6bd6b5cfb9c39e9b8acf973fe93
Bryson Kruk <bryson@Hyperion.local>
2015-04-28 06:25:21 -0600

    figured out how to add events

--- scripts/eventSync.js
+++ scripts/eventSync.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 	$("#calendar").clndr({
-		template: $('#calendar-template').html()
+		template: $('#calendar-template').html(),
+		daysOfTheWeek : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+		events : [
+			{date: '2015-05-10', title: 'Mothers Day'}
+		]
 	});
 });
