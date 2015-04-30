2dd843f5353bd7e4af28b1fd2a6a82a3da110c7d
Bryson Kruk <bryson@Hyperion.local>
2015-04-29 20:23:22 -0600

    Added tile toggling, event modal

--- scripts/eventSync.js
+++ scripts/eventSync.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
-	$("#calendar").clndr({
+	var calendar = $("#calendar").clndr({
 		template: $('#calendar-template').html(),
 		daysOfTheWeek : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
 		events : [
 			{date: '2015-05-10', title: 'Mothers Day'}
 		]
 	});
+
+	var EVENTHANDLERS = (function(){
+		
+
+		$("body").on('click', '.day', function(e){
+			if($(e.target).hasClass('fa') || $(e.target).hasClass('info_button_container')){
+				console.log($(this));
+				$("#eventDigestModal").modal();
+			}else{
+				$(this).toggleClass('selected');
+			}
+			
+		});
+	})();
 });
