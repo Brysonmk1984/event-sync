b76ab455774a6f31757bb3262a1091ab17780df3
Bryson Kruk <bryson@Hyperion.local>
2015-04-30 06:06:25 -0600

    figured out how to use clndr eventhandler, make it work with info button

--- scripts/eventSync.js
+++ scripts/eventSync.js
@@ -4,7 +4,25 @@ $(document).ready(function(){
 		daysOfTheWeek : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
 		events : [
 			{date: '2015-05-10', title: 'Mothers Day'}
-		]
+		],
+		clickEvents : {
+			click: function(target){
+				setTimeout(function(){
+					console.log(target.element);
+					var clickedOnInfo = $(target.element).hasClass('active_info');
+					
+					if(clickedOnInfo){
+						//Do stuff with day data
+						$("#eventDigestModal").modal();
+					}else{console.log(2);
+						$(this).toggleClass('selected');
+						// still need to remove class when modal closes
+					}
+
+				},100);
+				
+			}
+		}
 	});
 
 	var EVENTHANDLERS = (function(){
@@ -12,10 +30,7 @@ $(document).ready(function(){
 
 		$("body").on('click', '.day', function(e){
 			if($(e.target).hasClass('fa') || $(e.target).hasClass('info_button_container')){
-				console.log($(this));
-				$("#eventDigestModal").modal();
-			}else{
-				$(this).toggleClass('selected');
+				$(this).addClass('active_info');
 			}
 			
 		});
