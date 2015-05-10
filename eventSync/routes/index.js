8397797a815fdf81d97105afe5ee0953b2c923bd
Bryson Kruk <brysonmk1984@gmail.com>
2015-05-10 14:01:23 -0600

    can now add to DB, login checks cred

--- eventSync/routes/index.js
+++ eventSync/routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-   var db = req.db;
-    var collection = db.get('usercollection');
+  	//res.render('index', { title: 'Express' });
+   	var db = req.db;
+    var collection = db.get('users');
     collection.find({},{},function(e,docs){
         res.render('index', {
             "userlist" : docs
@@ -17,20 +17,58 @@ router.get('/', function(req, res, next) {
 API
 */
 
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
+/* Enroll in EventSync */
+router.post('/enroll', function(req, res) {
+	//Get form values
+	var db = req.db;
+	var userName = req.body.enrollUserName;
+	var email = req.body.enrollEmail;
+	var password = req.body.enrollPassword;
+	var color = req.body.enrollColor;
+
+	//Set our Collection
+	var collection = db.get('users');
+
+	//Submit to the DB
+	collection.insert({
+		"userName":userName,
+		"email":email,
+		"password":password,
+		"color":color
+	},function(err,doc){
+		if(err){
+			// If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+		}else{
+			// If it worked, set the header so the address bar doesn't still say /enroll
+            res.location("");
+            // And forward to success page
+            res.redirect("/");
+		}
+	});
+});
+
+
+/* Login to EventSync */
+router.post('/login', function(req, res) {
     var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
+    var userName = req.body.loginUserName;
+	var password = req.body.loginPassword;
+    var collection = db.get('users');
+    console.log(userName,password);
+    collection.find({"userName":userName,"password":password},{},function(e,docs){
+    	if(docs.length == 0){
+    		res.send("Your User Name or Password is incorrect.");
+    	}else{
+			// If it worked, set the header so the address bar doesn't still say /login
+	        res.location("");
+	        // And forward to success page
+	        res.redirect("/");
+	        console.log(docs);
+    	}
+		
     });
 });
 
-
-router.post('/enroll', function(req, res) {
-
-});
 
 module.exports = router;
